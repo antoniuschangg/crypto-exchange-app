f61bffec0c713f087dc85a14c9cb6fd7438b01cc
Antonius Chang <antonius.changg@gmail.com>
2024-07-21 23:48:02 +0700

    feat: add interval to klines chart

--- hooks/useKlinesChart.ts
+++ hooks/useKlinesChart.ts
@@ -13,21 +13,32 @@ const useKlinesChart = (symbol: string) => {
   useEffect(() => {
     const abortController = new AbortController();
 
-    fetch(`${BINANCE_API_BASE_URL}/uiKlines?symbol=${symbol}&interval=1d&limit=60`, { signal: abortController.signal })
-      .then((res) => res.json())
-      .then((data) => {
-        setChartData(
-          data.map((item: any) => ({
-            timestamp: Number(item[0]),
-            open: Number(item[1]),
-            high: Number(item[2]),
-            low: Number(item[3]),
-            close: Number(item[4]),
-          }))
-        );
-      });
+    const fetchData = () => {
+      fetch(`${BINANCE_API_BASE_URL}/uiKlines?symbol=${symbol}&interval=1d&limit=60`, { signal: abortController.signal })
+        .then((res) => res.json())
+        .then((data) => {
+          if (!abortController.signal.aborted) {
+            setChartData(
+              data.map((item: any) => ({
+                timestamp: Number(item[0]),
+                open: Number(item[1]),
+                high: Number(item[2]),
+                low: Number(item[3]),
+                close: Number(item[4]),
+              }))
+            );
+          }
+        });
+    };
+
+    fetchData();
+
+    const interval = setInterval(() => {
+      fetchData();
+    }, 5000);
 
     return () => {
+      clearInterval(interval);
       abortController.abort();
     };
   }, [symbol]);
